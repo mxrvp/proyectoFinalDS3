bc709b92785ef3873eca053f841da0c33bfe9240
max <mxrvp507@hotmail.com>
2022-07-09 14:42:42 -0500

    tcu

--- src/proyectofinalds3/ProyectoFinalDS3.java
+++ src/proyectofinalds3/ProyectoFinalDS3.java
@@ -4,14 +4,8 @@ package proyectofinalds3;
 public class ProyectoFinalDS3 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("hello world");
-    }
-    
-=======
         JFrameInSesion obj = new JFrameInSesion();
         obj.setVisible(true);
         
     }   
->>>>>>> 0f6be9468e0e2cfe06252fccd359b0051fdbe253
 }
